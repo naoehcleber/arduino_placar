3210c912345898b8ed56384b6a68ebe4d86581e0
Joao Victor <joao.vr.fernandes@gmail.com>
2024-02-05 22:08:47 -0300

    codigo certo

--- placar_tennis.ino
+++ placar_tennis.ino
@@ -1,44 +1,139 @@
-#include <Keyboard.h>
-const int Up = 2;
-const int Left = 3;
-const int Right = 4;
-const int Down = 5;
+#include "LedControl.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  
+const int DIN = 4;
+const int CS = 5;
+const int CLK = 6;
+
+const int BotaoEsquerda = 10;
+int buttonstateESQUERDA;
+
+LedControl lc=LedControl(DIN, CLK, CS,4);
+
+
+int placarDireito = 0;
+int placarEsquerdo = 0;
+
+void setup(){
   Serial.begin(9600);
-  // .begin();
-  pinMode(Up, INPUT_PULLUP);
-  pinMode(Down, INPUT_PULLUP);
-  pinMode(Left, INPUT_PULLUP);
-  pinMode(Right, INPUT_PULLUP);
+
+  pinMode(BotaoEsquerda, INPUT_PULLUP);
+  //inicializa o display
+  lc.shutdown(0, false);
+  lc.shutdown(1, false);
+  //Ajusta o brilho de cada display
+  lc.setIntensity(0, 8);
+  lc.setIntensity(1, 8);
+  lc.setIntensity(2, 8);
+  lc.setIntensity(3, 8);
+
+  //Apaga os displays
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  lc.clearDisplay(2);
+  lc.clearDisplay(3);
 }
 
+void zerarPlacar(int lado){
+  byte sprite_zero[8] = {B00000000,B01100110,B10011001,B10011001,B10011001,B10011001,B01100110,B00000000};
+  
+  lc.clearDisplay(lado);
 
-void loop() {
-  int buttonStateUp = digitalRead(Up);
-  int buttonStateDown = digitalRead(Down);
-  int buttonStateLeft = digitalRead(Left);
-  int buttonStateRight = digitalRead(Right);
-
-  if (buttonStateUp == LOW){
-    Keyboard.write('w');
-    Serial.println("w");
-    
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(lado, i, sprite_zero[i]);
   }
-  if (buttonStateDown == LOW){
-    Keyboard.write('s');
-    Serial.println("s");
-    
-  } if (buttonStateLeft == LOW){
-    Keyboard.write('a');
-    Serial.println("a");
-    
-  } if (buttonStateRight == LOW){
-    Keyboard.write('d');
-    Serial.println("d");
-    
-   
- 
 }
+
+void imprimirQuinze(int lado){
+  byte sprite_quinze[8] = {B00000000,B01000111,B01000100,B01000100,B01000111,B01000001,B11100111,B00000000};
+  lc.clearDisplay(lado);
+
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(lado, i, sprite_quinze[i]);
+  }
+  }
+
+void imprimitTrinta(int lado){
+  byte sprite_trinta[8] = {B00000000,B11100110,B00101001,B00101001,B11101001,B00101001,B11100110,B00000000};
+  lc.clearDisplay(lado);
+
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(lado, i, sprite_trinta[i]);
+  }
+
+}
+
+void imprimirQuarenta(int lado){
+  byte sprite_quarenta[8] = {B00000000,B10100110,B10101001,B10101001,B11101001,B00101001,B00100110,B00000000};
+  lc.clearDisplay(lado);
+
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(lado, i, sprite_quarenta[i]);
+  }
+}
+
+void imprimirGame(){
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  lc.clearDisplay(2);
+  lc.clearDisplay(3);
+
+  byte sprite_G[8] = {B00000000,B00111110,B01000000,B01000000,B01001110,B01000010,B00111110,B00000000};
+  byte sprite_A[8] = {B00000000,B00011000,B00100100,B01000010,B01000010,B01111110,B01000010,B00000000};
+  byte sprite_M[8] = {B00000000,B01100110,B01011010,B01011010,B01000010,B01000010,B01000010,B00000000};
+  byte sprite_E[8] = {B00000000,B01111110,B01000000,B01111000,B01000000,B01000000,B01111110,B00000000};
+
+
+}
+
+void loop(){
+  buttonstateESQUERDA = digitalRead(BotaoEsquerda);
+
+  zerarPlacar(0);
+  zerarPlacar(3);
+
+
+  if (buttonstateESQUERDA = LOW){
+    placarEsquerdo += 1;
+    Serial.println(placarEsquerdo);
+
+  } 
+  switch (placarEsquerdo){
+    case 1:
+      imprimirQuinze(3);
+      break;
+    case 2:
+      imprimitTrinta(3);
+      break;
+    case 3:
+      imprimirQuarenta(3);
+      break;
+    case 4 :
+      imprimirGame();
+      zerarPlacar(3);
+      zerarPlacar(0);
+
+      break;
+  }
+  switch (placarDireito){
+    case 1:
+      imprimirQuinze(0);
+      break;
+    case 2:
+      imprimitTrinta(0);
+      break;
+    case 3:
+      imprimirQuarenta(0);
+      break;
+    case 4 :
+      imprimirGame();
+      zerarPlacar(3);
+      zerarPlacar(0);
+      break;
+  }
+
+
+} 
