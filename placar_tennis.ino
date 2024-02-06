534fc40da9c07d91839d65a32fedb135d76557bf
naoehcleber <80178198+naoehcleber@users.noreply.github.com>
2024-02-06 00:33:01 -0300

    codigo

--- placar_tennis.ino
+++ placar_tennis.ino
@@ -9,7 +9,6 @@ int buttonstateESQUERDA;
 
 LedControl lc=LedControl(DIN, CLK, CS,4);
 
-
 int placarDireito = 0;
 int placarEsquerdo = 0;
 
@@ -20,12 +19,13 @@ void setup(){
   //inicializa o display
   lc.shutdown(0, false);
   lc.shutdown(1, false);
+  lc.shutdown(2, false);
+  lc.shutdown(3, false);
   //Ajusta o brilho de cada display
   lc.setIntensity(0, 8);
   lc.setIntensity(1, 8);
   lc.setIntensity(2, 8);
   lc.setIntensity(3, 8);
-
   //Apaga os displays
   lc.clearDisplay(0);
   lc.clearDisplay(1);
@@ -36,8 +36,6 @@ void setup(){
 void zerarPlacar(int lado){
   byte sprite_zero[8] = {B00000000,B01100110,B10011001,B10011001,B10011001,B10011001,B01100110,B00000000};
   
-  lc.clearDisplay(lado);
-
   for(int i = 0; i<8; i++){
     //lc.setRow(lado, row, posicao no vetor)
     lc.setRow(lado, i, sprite_zero[i]);
@@ -45,24 +43,21 @@ void zerarPlacar(int lado){
 }
 
 void imprimirQuinze(int lado){
-  byte sprite_quinze[8] = {B00000000,B01000111,B01000100,B01000100,B01000111,B01000001,B11100111,B00000000};
-  lc.clearDisplay(lado);
-
+  byte sprite_quinze[8] = {B00000000,B01000111,B11000100,B01000100,B01000111,B01000001,B11100111,B00000000};
+  
   for(int i = 0; i<8; i++){
     //lc.setRow(lado, row, posicao no vetor)
     lc.setRow(lado, i, sprite_quinze[i]);
   }
-  }
+}
 
 void imprimitTrinta(int lado){
   byte sprite_trinta[8] = {B00000000,B11100110,B00101001,B00101001,B11101001,B00101001,B11100110,B00000000};
-  lc.clearDisplay(lado);
 
   for(int i = 0; i<8; i++){
     //lc.setRow(lado, row, posicao no vetor)
     lc.setRow(lado, i, sprite_trinta[i]);
   }
-
 }
 
 void imprimirQuarenta(int lado){
@@ -86,54 +81,95 @@ void imprimirGame(){
   byte sprite_M[8] = {B00000000,B01100110,B01011010,B01011010,B01000010,B01000010,B01000010,B00000000};
   byte sprite_E[8] = {B00000000,B01111110,B01000000,B01111000,B01000000,B01000000,B01111110,B00000000};
 
-
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(3, i, sprite_G[i]);
+  }
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(2, i, sprite_A[i]);
+  }
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(1, i, sprite_M[i]);
+  }
+  for(int i = 0; i<8; i++){
+    //lc.setRow(lado, row, posicao no vetor)
+    lc.setRow(0, i, sprite_E[i]);
+  }
 }
 
 void loop(){
   buttonstateESQUERDA = digitalRead(BotaoEsquerda);
 
-  zerarPlacar(0);
-  zerarPlacar(3);
-
-
-  if (buttonstateESQUERDA = LOW){
+  if (buttonstateESQUERDA == HIGH){
     placarEsquerdo += 1;
     Serial.println(placarEsquerdo);
+    while(digitalRead(BotaoEsquerda) == HIGH){}
+  }
 
-  } 
   switch (placarEsquerdo){
+    case 0:
+      lc.clearDisplay(3);
+      zerarPlacar(3);
+      break;
     case 1:
+      lc.clearDisplay(3);
       imprimirQuinze(3);
       break;
     case 2:
+      lc.clearDisplay(3);
       imprimitTrinta(3);
       break;
     case 3:
+      lc.clearDisplay(3);
       imprimirQuarenta(3);
       break;
     case 4 :
+      placarDireito = 4;
+      lc.clearDisplay(3);
       imprimirGame();
-      zerarPlacar(3);
-      zerarPlacar(0);
-
       break;
-  }
+    case 5:
+      if(placarEsquerdo == 5){
+        placarDireito = 5;
+      }
+      zerarPlacar(3);
+      lc.clearDisplay(2);
+      lc.clearDisplay(1);
+      //zerarPlacar(0);
+      placarEsquerdo = 0;
+      placarDireito = 0;
+      break;
+    }
+  
   switch (placarDireito){
+    case 0:
+      lc.clearDisplay(0);
+      zerarPlacar(0);
+      break;
     case 1:
+      lc.clearDisplay(0);
       imprimirQuinze(0);
       break;
     case 2:
+      lc.clearDisplay(0);
       imprimitTrinta(0);
       break;
     case 3:
+      lc.clearDisplay(0);
       imprimirQuarenta(0);
       break;
     case 4 :
+      lc.clearDisplay(0);
       imprimirGame();
-      zerarPlacar(3);
-      zerarPlacar(0);
       break;
-  }
-
-
+    case 5:
+      zerarPlacar(3);
+      lc.clearDisplay(2);
+      lc.clearDisplay(1);
+      zerarPlacar(0);
+      //placarEsquerdo = 0;
+      break;
+    }
 } 
